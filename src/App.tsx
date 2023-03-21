588927858b5bd1690c8a3608bcbdd483fa507070
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2023-03-21 11:22:40 +0100

    börjar bygga app 19:42

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1 className="heading">Taskify</h1>
       </header>
     </div>
   );
-}
+};
+
+// Typescript typer och sätt att skriva dem
+// type X = {
+//   a: string;
+//   b: number;
+// };
+
+// type Y = X & {
+//   c: string;
+//   d: number;
+// };
+
+// let xy: Y = {
+//   // type(s) går att smälta ihop. Nu behöver man skriva alla abcd keys för att färdigställa xy variabeln. "Extending types"
+//   a: "hej",
+//   b: 11,
+//   c: "grej",
+//   d: 42,
+// };
+// interface Person {
+//   namnet: string;
+//   alder: number;
+// }
+// interface Guy extends Person {
+//   profession: string;
+//   experience: number;
+// }
+// let kille: Guy = {
+//   profession: "it",
+//   experience: 3,
+//   namnet: "kalle",
+//   alder: 42,
+// };
+// console.log(kille);
+//  Går att extenda "interface" med "type", vice versa går det att använda "&" för att extenda "type" med "interface".
+
+// let namn: string;
+// let alder: number;
+// let sifferbokstav: string | number; // union, variabel kan vara både och nu.
+// let estudente: boolean;
+// let hobbies: string[];
+// let rollen: [string, number, boolean]; // Tuple, fixed number of variables in array.
+// type Person = {
+//   namnet: string;
+//   alder?: number;
+// };
+// let person1: Person = {
+//   namnet: "Ackman",
+//   alder: 42,
+// };
+// let flerapersoner: Person[];
+// let printfunction: (printas:string) => void; // type function som ska ta emot string.
+// let personName:unknown; // bra att skriva ifall man inte vet vad för typ en variabel kmr bli. annars skriv :any
 
 export default App;
