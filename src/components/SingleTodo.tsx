7d80a5c2b764d244e196f3a64ac76f385fed3bc9
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2023-03-24 11:00:45 +0100

    Singletodo Edit/Done/delete functionaly created

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { Todo } from "../models/models";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
@@ -15,18 +15,71 @@ const SingleTodo: React.FC<SingleTodoProps> = ({
   todos,
   setTodos,
 }: SingleTodoProps) => {
+  // const handleDone = (id: number) => {
+  //   todos.map((todo) => {
+  //     if (todo.id === id) {
+  //       todo.isDone = !todo.isDone;      Min lösning på uppgiften. Funkar som ska. boolean ändrar värde.
+  //     }                                  Använder hans lösning dock för att vi använder useState "setTodos()" vilket är bättre.
+  //   });
+  // };
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, seteditTodo] = useState<string>(todo.task);
+
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleEdit = (e: React.FormEvent<HTMLFormElement>, id: number) => {
+    e.preventDefault();
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, task: editTodo } : todo))
+    );
+    setEdit(!edit);
+  };
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [edit]);
+
   return (
-    <form className="todos__single">
-      <span className="todos__single--text">{todo.task}</span>
+    <form className="todos__single" onSubmit={(e) => handleEdit(e, todo.id)}>
+      {edit ? (
+        <input
+          ref={inputRef}
+          value={editTodo}
+          onChange={(e) => seteditTodo(e.target.value)}
+          className="todos__single--text"
+        />
+      ) : todo.isDone ? (
+        <s className="todos__single--text">{todo.task}</s>
+      ) : (
+        <span className="todos__single--text">{todo.task}</span>
+      )}
 
       <div>
-        <span className="icon">
+        <span
+          className="icon"
+          onClick={() => {
+            if (!edit && !todo.isDone) {
+              setEdit(!edit);
+            }
+          }}
+        >
           <AiFillEdit />
         </span>
-        <span className="icon">
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
           <AiFillDelete />
         </span>
-        <span className="icon">
+        <span className="icon" onClick={() => handleDone(todo.id)}>
           <MdDone />
         </span>
       </div>
