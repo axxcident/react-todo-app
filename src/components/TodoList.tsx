e8cd4be293168dca862509df2a51b24a24bfa282
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2023-03-26 10:14:09 +0200

    css building UI

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -5,20 +5,39 @@ import SingleTodo from "./SingleTodo";
 
 interface TodosProps {
   todos: Array<Todo>;
-  setTodos: React.Dispatch<React.SetStateAction<Array<Todo>>>;
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
+// setTodos: React.Dispatch<React.SetStateAction<Array<Todo>>>;
 
 const TodoList: React.FC<TodosProps> = ({ todos, setTodos }: TodosProps) => {
   return (
-    <div className="todos">
-      {todos.map((todo) => (
-        <SingleTodo
-          key={todo.id}
-          todo={todo}
-          todos={todos}
-          setTodos={setTodos}
-        />
-      ))}
+    <div className="container">
+      <div className="todos">
+        <span className="todos__heading">Active Tasks</span>
+        {todos.map((todo) => {
+          return (
+            <SingleTodo
+              key={todo.id}
+              todo={todo}
+              todos={todos}
+              setTodos={setTodos}
+            />
+          );
+        })}
+      </div>
+      <div className="todos remove">
+        <span className="todos__heading">Completed Tasks</span>
+        {todos.map((todo) => {
+          return (
+            <SingleTodo
+              key={todo.id}
+              todo={todo}
+              todos={todos}
+              setTodos={setTodos}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
